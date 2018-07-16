6e5b2dd2d3b119930fd38c21bcf8badc2d09236c
Graham <grahamspence@gmail.com>
2018-07-16 15:16:49 -0400

    exercise 3 complete

--- exercise3.rb
+++ exercise3.rb
@@ -1,1 +1,6 @@
 puts "What is your name?"
+  user_name = gets.chomp
+puts "Hello, #{user_name}. How old are you?"
+  age = gets.chomp.to_i
+  bday = (2018 - age)
+puts "That means you were born in #{bday}!"
