1e7db1f479abf3ae7c42e9178c9fa0cab07e896b
Graham <grahamspence@gmail.com>
2018-07-16 14:37:51 -0400

    exercise 2 complete

---
+++ exercise2.rb
@@ -0,0 +1,25 @@
+# How would you calculate a good tip for a 55 dollar meal? Use puts to print the answer.
+#
+# puts "Want to leave a good tip? How much did your meal cost?"
+#
+# total = gets.chomp.to_i
+# tip = 0.20
+#
+# puts "You should pay $#{total * tip}."
+#
+#
+# # Try adding a string and an integer with the + operator. What happens? Find a way to convert the integer into a string first and use puts to print the result.
+#
+# puts "Hello World " + "5000"
+#
+#
+# # Try outputting the result of 45628 multiplied by 7839 in a sentence by using string interpolation.
+#
+# total = 45628 * 7839
+#
+# puts "I love big numbers like #{total}!"
+#
+# # What's the value of the expression (10 < 20 && 30 < 20) || !(10 == 11)? Try figuring it out on your own before typing it in.
+#
+# (false and false) or false(false) = true
+# Correct!
